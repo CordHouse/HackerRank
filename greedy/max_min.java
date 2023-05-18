baa2a6aaedc312aaba67ccd2d25a9d415ae06156
CordHouse <qkqktl5310@gmail.com>
2023-05-19 00:03:01 +0900

    2023.05.18 max_min

--- greedy/max_min.java
+++ greedy/max_min.java
@@ -1,4 +1,63 @@
 package greedy;
+import java.io.*;
+import java.util.*;
+import java.util.stream.IntStream;
+
+import static java.util.stream.Collectors.toList;
 
 public class max_min {
+    static class Result {
+
+        /*
+         * Complete the 'maxMin' function below.
+         *
+         * The function is expected to return an INTEGER.
+         * The function accepts following parameters:
+         *  1. INTEGER k
+         *  2. INTEGER_ARRAY arr
+         */
+
+        public static int maxMin(int k, List<Integer> arr) {
+            // Write your code here
+            Collections.sort(arr);
+
+            int answer = Integer.MAX_VALUE;
+            for(int i = 0; i <= arr.size()-k; i++) {
+                int max = arr.get(i+k-1);
+                int min = arr.get(i);
+                answer = Math.min(answer, max-min);
+            }
+
+            return answer;
+        }
+
+    }
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        int n = Integer.parseInt(bufferedReader.readLine().trim());
+
+        int k = Integer.parseInt(bufferedReader.readLine().trim());
+
+        List<Integer> arr = IntStream.range(0, n).mapToObj(i -> {
+                    try {
+                        return bufferedReader.readLine().replaceAll("\\s+$", "");
+                    } catch (IOException ex) {
+                        throw new RuntimeException(ex);
+                    }
+                })
+                .map(String::trim)
+                .map(Integer::parseInt)
+                .collect(toList());
+
+        int result = Result.maxMin(k, arr);
+
+        bufferedWriter.write(String.valueOf(result));
+        bufferedWriter.newLine();
+
+        bufferedReader.close();
+        bufferedWriter.close();
+    }
+
 }
