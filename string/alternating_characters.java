41bd75280b91098eda3431381f76f98f66389f00
CordHouse <qkqktl5310@gmail.com>
2023-05-19 16:28:29 +0900

    2023.05.19 alternating_characters

---
+++ string/alternating_characters.java
@@ -0,0 +1,60 @@
+package string;
+
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.function.*;
+import java.util.regex.*;
+import java.util.stream.*;
+import static java.util.stream.Collectors.joining;
+import static java.util.stream.Collectors.toList;
+public class alternating_characters {
+    /*
+     * Complete the 'alternatingCharacters' function below.
+     *
+     * The function is expected to return an INTEGER.
+     * The function accepts STRING s as parameter.
+     */
+
+    public static int alternatingCharacters(String s) {
+        // Write your code here
+        int curPoint = 0;
+        int nextPoint = 0;
+        int count = 0;
+        for(int i = 0; i < s.length()-1; i++) {
+            curPoint = i;
+            nextPoint = i+1;
+            if(s.charAt(curPoint) == s.charAt(nextPoint)) {
+                count++;
+            }
+        }
+
+        return count;
+    }
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        int q = Integer.parseInt(bufferedReader.readLine().trim());
+
+        IntStream.range(0, q).forEach(qItr -> {
+            try {
+                String s = bufferedReader.readLine();
+
+                int result = alternatingCharacters(s);
+
+                bufferedWriter.write(String.valueOf(result));
+                bufferedWriter.newLine();
+            } catch (IOException ex) {
+                throw new RuntimeException(ex);
+            }
+        });
+
+        bufferedReader.close();
+        bufferedWriter.close();
+    }
+
+}
